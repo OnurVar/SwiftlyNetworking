9a23f094cffe92b8ae7da36f6e4d58b8e2098b9d
Onur Var <var.onur@hotmail.com>
2023-11-22 05:30:59 +0300

    improved query parameters

--- Package.swift
+++ Package.swift
@@ -14,7 +14,6 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/OnurVar/SwiftlyHelper.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     ],
     targets: [
@@ -23,7 +22,6 @@ let package = Package(
         .target(
             name: "SwiftlyNetworking",
             dependencies: [
-                "SwiftlyHelper",
                 .product(name: "Logging", package: "swift-log"),
             ]),
         .testTarget(
